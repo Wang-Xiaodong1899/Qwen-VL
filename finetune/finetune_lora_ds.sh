e55bc4acb37d1f4d99bc2e9affb5e0f8863c8104
Xiaodong Wang <v-xiaodwang@microsoft.com>
2023-09-28 15:29:31 +0800

    fix

--- finetune/finetune_lora_ds.sh
+++ finetune/finetune_lora_ds.sh
@@ -8,7 +8,7 @@ NODE_RANK=0
 MASTER_ADDR=localhost
 MASTER_PORT=6001
 
-MODEL="/f_data/G/Qwen/Qwen-VL-Chat" #"Qwen/Qwen-VL-Chat"/"Qwen/Qwen-VL"  Set the path if you do not want to load from huggingface directly
+MODEL="/f_data/G/Qwen/Qwen-VL" #"Qwen/Qwen-VL-Chat"/"Qwen/Qwen-VL"  Set the path if you do not want to load from huggingface directly
 # ATTENTION: specify the path to your training data, which should be a json file consisting of a list of conversations.
 # See the section for finetuning in README for more information.
 
